3ab8e36f297062b174b5b2308a6045b4636cd751
Michael Herman <hermanmu@gmail.com>
2017-07-09 12:01:14 -0600

    updated config for staging env

--- project/config.py
+++ project/config.py
@@ -32,6 +32,12 @@ class TestingConfig(BaseConfig):
     TOKEN_EXPIRATION_SECONDS = 3
 
 
+class StagingConfig(BaseConfig):
+    """Staging configuration"""
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
 class ProductionConfig(BaseConfig):
     """Production configuration"""
     DEBUG = False
